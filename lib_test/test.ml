ca1fd61fdfeb7f92cab8886babfa5b00b1145c73
Jeremy Yallop <yallop@gmail.com>
2016-01-05 15:06:58 +0000

    Don't rely on readdir ordering in the tests.

--- lib_test/test.ml
+++ lib_test/test.ml
@@ -12,6 +12,25 @@ open OUnit2
 let id x = x
 
 
+module StringSet = Set.Make(String)
+
+(** The contents of test-directory/ as a set *)
+let test_directory_contents =
+  Array.fold_right StringSet.add 
+    (Sys.readdir "test-directory")
+    (StringSet.(union (singleton "..") (singleton ".")))
+
+let string_of_stringset set =
+  "{ "^ StringSet.fold (fun x y -> x ^" "^ y) set "}"
+
+let assert_remove item items =
+  let msg = Printf.sprintf "%s is in %s" item (string_of_stringset items) in
+  begin
+    assert_bool msg (StringSet.mem item items);
+    StringSet.remove item items
+  end
+
+
 (**
  * Test that [file_descr] values are represented as [int]s.
  *)
@@ -58,19 +77,11 @@ let test_dir_handle_round_trip _ =
   let dh = Unix.opendir "test-directory" in
   let dh_int = Unix_representations.nativeint_of_dir_handle dh in
   let dh' = Unix_representations.dir_handle_of_nativeint dh_int in
-  begin
-    assert_equal ".."
-      (Unix.readdir dh)
-      ~printer:id;
-
-    assert_equal "a"
-      (Unix.readdir dh')
-      ~printer:id;
-
-    assert_equal "b"
-      (Unix.readdir dh)
-      ~printer:id;
-  end
+  let entries = test_directory_contents in
+  let entries = assert_remove (Unix.readdir dh) entries in
+  let entries = assert_remove (Unix.readdir dh') entries in
+  let entries = assert_remove (Unix.readdir dh) entries in
+  ()
 
 
 (**
@@ -112,17 +123,10 @@ let test_use_dir_handle_with_ctypes _ =
   let dir_ptr = ctypes_opendir "test-directory" in
   let raw_ptr = Ctypes.raw_address_of_ptr dir_ptr in
   let dh = Unix_representations.dir_handle_of_nativeint raw_ptr in
-  begin
-    assert_equal ".."
-      (Unix.readdir dh)
-      ~printer:id;
-
-    assert_equal "a"
-      (Unix.readdir dh)
-      ~printer:id;
-
-    Unix.closedir dh;
-  end;
+  let entries = test_directory_contents in
+  let entries = assert_remove (Unix.readdir dh) entries in
+  let entries = assert_remove (Unix.readdir dh) entries in
+  Unix.closedir dh;
 
   (* Open a directory with Unix and close it with Ctypes *)
   let dh = Unix.opendir "test-directory" in
